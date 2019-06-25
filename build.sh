6c6d4c00fba1e8ecd625bbabf49ab147b80cff55
Ilya Sevrugin <isevrugin@shutterstock.com>
2019-06-25 11:53:11 -0700

    build for heroku

--- build.sh
+++
@@ -1,8 +0,0 @@
-rm -rf /tmp/nastya_package;
-npm run clean && grunt client;
-cp -a ./. /tmp/nastya_package/;
-cd /tmp/nastya_package/;
-rm -rf .idea/ node_modules/ npm-debug.log package.zip package/;
-zip -r package.zip *;
-cd -;
-cp /tmp/nastya_package/package.zip ./
